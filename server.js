be914f9d72d664cab02a69112a2f9ab85ba1ad0e
maxiemar <denishicmihail@gmail.com>
2022-02-03 12:54:30 +0300

    feat: send response

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const serverPort = 8080;
 
 const server = http.createServer((request, response) => {
 	console.log(`Accepted request: url=${request.url}, method=${request.method}`);
+	response.setHeader("Content-Type", "text/plain");
+	response.write("Hello, world");
+	response.end();
 });
 
 server.listen(serverPort, () => {
