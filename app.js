00928597ab6e25e919bb680ef422a50fc61ce624
maxiemar <denishicmihail@gmail.com>
2022-02-19 14:19:30 +0300

    build: use express

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const serverHost = "localhost";
+const serverPort = 8080;
+
+const app = express();
+
+app.listen(serverPort, serverHost, () => {
+	console.log(`Server started listening on ${serverHost}:${serverPort}`);
+})
+
+app.get("/", (request, response) => {
+	response.sendFile("/views/welcome.html", { root: __dirname });
+})
+
+app.get("/welcome", (request, response) => {
+	response.sendFile("/views/welcome.html", { root: __dirname });
+})
+
+app.get("/about", (request, response) => {
+	response.sendFile("/views/about.html", { root: __dirname });
+})
+
+app.use((request, response) => {
+	response.sendFile("./views/error404.html", { root: __dirname });
+})
