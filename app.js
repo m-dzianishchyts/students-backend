5dc794a1e08e686ad5f7494e060e2caa418fac7b
Mikhail Dzianishchyts <mikhail.dzianishchyts@gmail.com>
2022-02-24 02:31:30 +0300

    build: use morgan for logging

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
 const express = require("express");
-const config = require("./config")
+const morgan = require("morgan");
+
+const config = require("./config");
 
 const app = express();
-
 app.set("view engine", "ejs");
+
+app.use(morgan("dev"));
 
 app.listen(config.port, () => {
 	console.log(`Server started listening on port ${config.port}`);
-})
+});
 
 app.get("/", (request, response) => {
 	response.render("overview", {
-		title: "Student Group Queues & Notes"
+		title: "Student Group Queues & Notes",
 	});
-})
+});
 
 app.get("/overview", (request, response) => {
 	response.render("overview", {
-		title: "Student Group Queues & Notes"
+		title: "Student Group Queues & Notes",
 	});
-})
+});
 
 app.get("/contacts", (request, response) => {
 	response.render("contacts", {
@@ -28,12 +31,12 @@ app.get("/contacts", (request, response) => {
 			{ name: "GitHub", url: "https://github.com/m-dzianishchyts/students" },
 			{ name: "Email", url: "mailto:mikhail.dzianishchyts@gmail.com" },
 			{ name: "LinkedIn", url: "https://www.linkedin.com/in/mikhail-dzianishchyts/" },
-		]
+		],
 	});
-})
+});
 
 app.use((request, response) => {
 	response.status(404).render("error404", {
-		title: "Error"
+		title: "Error",
 	});
 });
